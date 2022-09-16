6a2dd0fb48ff18a2b0bb18b2e420aa0d7eb0f0af
DSlundro <dragan.savic95@outlook.it>
2022-09-16 15:23:50 +0200

    add bookmarks view + refactoring

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,18 +1,15 @@
-import { async } from 'regenerator-runtime';
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import searchResultsView from './views/searchResultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
 ///////////////////////////////////////
-
-/* if(module.hot){
-  module.hot.accept()
-} */
 
 const controlRecipes = async function() {
   try {
@@ -24,6 +21,7 @@ const controlRecipes = async function() {
 
     // 0) Update results view to mark selected search result
     searchResultsView.render(model.getSearchResultsPage())
+    bookmarksView.render(model.state.bookmarks)
     
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -75,10 +73,13 @@ const controlServings = function(newServings) {
 }
 
 const controlAddBookmark = function() {
+  // 1) Add/remove bookmark
   if(!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.removeBookmark(model.state.recipe.id);
-
-  recipeView.render(model.state.recipe)
+  // 2) Update recipe view
+  recipeView.render(model.state.recipe);
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 }
 
 const init = function() {
