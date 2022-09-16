f6c8bc44bd729f056a81ceabf44761b27fb66cc0
DSlundro <dragan.savic95@outlook.it>
2022-09-16 23:35:31 +0200

    add sezione 19

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -2,13 +2,20 @@ import icons from 'url:../../img/icons.svg';
 
 export default class View {
     _data;
-
+    /**
+     * Render the received object to teh DOM
+     * @param {Object | Object[]} data The data to be rendered (e.g recipe)
+     * @param {boolean} [render=true] If false, create markup string instead of rendering to the DOM
+     * @returns {undefined | string} A markup string is returned if render=false
+     * @this {Object} View instance
+     * @author Dragan Savic
+     */
     render(data, render = true) {
         if(!data || (Array.isArray(data) && data.length === 0))  return this.renderError();
 
         this._data = data;
         const markup = this._generateMarkup();
-        
+
         if(!render) return markup;
         this._clear();
         this._parentElement.insertAdjacentHTML('afterbegin', markup);
